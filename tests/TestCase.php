d2457a0ba5df8c7367899e20a089b3d5eb3fe1b1
Alexandr <lesandrwhite@gmail.com>
2020-03-10 15:35:14 +0200

    Tests and Guide 3/3

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
